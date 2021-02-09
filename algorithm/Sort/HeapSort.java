010a32ed354a284fe7d84820f23fa3a38c5dccff
kimwonhee626 <kwh7739@gmail.com>
2021-02-09 15:27:45 +0900

    Update HeapSort.java

--- algorithm/Sort/HeapSort.java
+++ algorithm/Sort/HeapSort.java
@@ -1,15 +1,16 @@
 
 public class HeapSort {
 	public void heapSort(int[] arr) {
-		downHeap(arr, 1, arr.length);
-		
+		int size = arr.length - 1;
+		buildHeap(arr, size); // 주어진 배열 최대힙으로 만듬
+		sort(arr, size);
 		
 	}
 	
 	public void downHeap(int[] arr, int i, int size) {
 		int left = i * 2;
 		int right = i * 2 + 1;
-		int greater;
+		int greater = i;
 		
 		if(left > size) { // 자식이 없는 노드일때
 			return;
@@ -22,25 +23,53 @@ public class HeapSort {
 			}
 		}
 		
-		if(arr[i] >= arr[greater]) {
-			return;
+		if(arr[i] < arr[greater]) { // 자식들보다 부모가 작으면 교환
+			swap(arr, i, greater); // 더 큰 자식과 swap
+			downHeap(arr, greater, size);
 		}
-		
+	}
+	
+	// 최대힙 만들기
+	public void buildHeap(int[] arr, int size) {
+		for(int i = size / 2; i > 0; i--) {
+			downHeap(arr, i, size);
+		}
+	}
+	
+	public void swap(int[] arr, int i, int j) {
 		int temp = arr[i];
-		arr[i] = arr[greater];
-		arr[greater] = temp;
-		
-		downHeap(arr, greater, size);
-		
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+	
+	public void sort(int[]arr, int size) { // 힙정렬
+		for(int i=size; i > 1; i--) {
+			swap(arr, 1, i); // 마지막 노드와 루트 swap
+			buildHeap(arr, i-1); // 루트노드를 기준으로 최대힙 만들기
+			
+			print(arr);
+		}
+	}
+	
+	public void print(int[] arr) {
+		for(int i=1; i<arr.length ; i++) {
+			
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
 	}
 	
 	
 	
-	
-	
-	
 	public static void main(String[] args) {
-
+		// 배열 인덱스 1부터 시작
+		int[] arr = new int[] {-1, 2, 5, 4, 1, 9, 8, 7, 6, 3, 0};
+		
+		HeapSort heap = new HeapSort();
+		
+		heap.print(arr);
+		heap.heapSort(arr);
+		heap.print(arr);
 	}
 	
 }
